cb68f1acb1a78ca0fe6dbf6df13763f57b1480d6
NeboT1000 <73859153+NeboT1000@users.noreply.github.com>
2021-10-18 22:40:56 +0200

    index html file changes on load as instructed!

--- src/js/app.js
+++ src/js/app.js
@@ -36,3 +36,66 @@ const news = [
  */
 
  console.log('js loaded!')
+
+ //EX01
+const ex01 = document.querySelector('#ex01').textContent = 'done!'
+
+//EX02
+const ex02 = document.querySelector('.ex02')
+ex02.classList.add('red')
+
+//EX03
+const ex03 = document.querySelector('blockquote').textContent = 'An apple a day makes the doctor go away'
+
+//EX04
+const ex04 = document.querySelector('#shopping-list')
+let li = document.createElement('li')
+li.appendChild(document.createTextNode('Milk'))
+ex04.appendChild(li)
+
+//EX05
+const ex05 = document.querySelector('#news')
+ex05.appendChild(document.createElement('ul'))
+let id = 1
+news.forEach(
+    n => {
+            const li = document.createElement('li')
+            const header = document.createElement('h1')
+            const author = document.createElement('h2')
+            const a = document.createElement('a')
+            const p = document.createElement('p')
+            const t1 = document.createTextNode(n.headline)
+            const t2 = document.createTextNode(n.author)
+            const t3 = document.createTextNode(n.href)
+            const t4 = document.createTextNode(n.summary)
+            header.appendChild(t1)
+            header.className = `header-${id++}`
+            author.appendChild(t2)
+            a.appendChild(a.href = t3)
+            p.appendChild(t4)
+            li.appendChild(header)
+            li.appendChild(author)
+            li.appendChild(a)
+            li.appendChild(p)
+            ex05.appendChild(li)
+    }
+)
+const header1 = document.querySelector('.header-1')
+header1.classList.add('headerstyle')
+
+
+//EX06
+let counter = 1
+const ex06 = document.addEventListener('click', () => {document.querySelector('#click-counter').innerHTML = counter++})
+
+//EX07
+const btn = document.querySelector('button')
+btn.addEventListener('click', () => {btn.disabled = true})
+
+//EX08
+const link = document.querySelector('#link-to-svt')
+link.addEventListener('click', (event) => {event.preventDefault()})
+
+//EX09
+const img = document.querySelector('img')
+img.src = 'https://images.unsplash.com/photo-1544982503-9f984c14501a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80'
